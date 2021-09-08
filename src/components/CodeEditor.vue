e07dc3542b82ff96bb704f7402f9f752a23c5241
shangbin <shangbin1@tal.com>
2021-09-08 20:22:52 +0800

    编辑器跑通,增加axios

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,171 +1,173 @@
 <template>
-  <div class="example">
-    <div class="codemirror">
-      <codemirror
-        v-model="code" 
-        :options="cmOption"
-        @cursorActivity="onCmCursorActivity"
-        @ready="onCmReady"
-        @focus="onCmFocus"
-        @blur="onCmBlur"
-      />
-    </div>
+  <div class="codemirror">
+    <codemirror v-model="code" :options="cmOption" @cursorActivity="onCmCursorActivity" @ready="onCmReady"
+      @focus="onCmFocus" @blur="onCmBlur" />
   </div>
 </template>
 
 <script>
-  import dedent from 'dedent'
-  import { codemirror } from 'vue-codemirror'
+import dedent from 'dedent'
+import { codemirror } from 'vue-codemirror'
 
-  // base style
-  import 'codemirror/lib/codemirror.css'
+// base style
+import 'codemirror/lib/codemirror.css'
 
-  // theme css
-  import 'codemirror/theme/monokai.css'
+// theme css
+import 'codemirror/theme/monokai.css'
 
-  // language
-  import 'codemirror/mode/vue/vue.js'
+// language
+import 'codemirror/mode/vue/vue.js'
 
-  // active-line.js
-  import 'codemirror/addon/selection/active-line.js'
+// active-line.js
+import 'codemirror/addon/selection/active-line.js'
 
-  // styleSelectedText
-  import 'codemirror/addon/selection/mark-selection.js'
-  import 'codemirror/addon/search/searchcursor.js'
+// styleSelectedText
+import 'codemirror/addon/selection/mark-selection.js'
+import 'codemirror/addon/search/searchcursor.js'
 
-  // highlightSelectionMatches
-  import 'codemirror/addon/scroll/annotatescrollbar.js'
-  import 'codemirror/addon/search/matchesonscrollbar.js'
-  import 'codemirror/addon/search/searchcursor.js'
-  import 'codemirror/addon/search/match-highlighter.js'
+// highlightSelectionMatches
+import 'codemirror/addon/scroll/annotatescrollbar.js'
+import 'codemirror/addon/search/matchesonscrollbar.js'
+import 'codemirror/addon/search/searchcursor.js'
+import 'codemirror/addon/search/match-highlighter.js'
 
-  // keyMap
-  import 'codemirror/mode/clike/clike.js'
-  import 'codemirror/addon/edit/matchbrackets.js'
-  import 'codemirror/addon/comment/comment.js'
-  import 'codemirror/addon/dialog/dialog.js'
-  import 'codemirror/addon/dialog/dialog.css'
-  import 'codemirror/addon/search/searchcursor.js'
-  import 'codemirror/addon/search/search.js'
-  import 'codemirror/keymap/sublime.js'
+// keyMap
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/addon/edit/matchbrackets.js'
+import 'codemirror/addon/comment/comment.js'
+import 'codemirror/addon/dialog/dialog.js'
+import 'codemirror/addon/dialog/dialog.css'
+import 'codemirror/addon/search/searchcursor.js'
+import 'codemirror/addon/search/search.js'
+import 'codemirror/keymap/sublime.js'
 
-  // foldGutter
-  import 'codemirror/addon/fold/foldgutter.css'
-  import 'codemirror/addon/fold/brace-fold.js'
-  import 'codemirror/addon/fold/comment-fold.js'
-  import 'codemirror/addon/fold/foldcode.js'
-  import 'codemirror/addon/fold/foldgutter.js'
-  import 'codemirror/addon/fold/indent-fold.js'
-  import 'codemirror/addon/fold/markdown-fold.js'
-  import 'codemirror/addon/fold/xml-fold.js'
+// foldGutter
+import 'codemirror/addon/fold/foldgutter.css'
+import 'codemirror/addon/fold/brace-fold.js'
+import 'codemirror/addon/fold/comment-fold.js'
+import 'codemirror/addon/fold/foldcode.js'
+import 'codemirror/addon/fold/foldgutter.js'
+import 'codemirror/addon/fold/indent-fold.js'
+import 'codemirror/addon/fold/markdown-fold.js'
+import 'codemirror/addon/fold/xml-fold.js'
 
+require(['axios'], axios => {
+  axios.defaults.baseURL = '';
+  self.axios = axios;
+});
+
+
+export default {
+  name: 'code-editor',
+  title: 'Mode: text/x-vue & Theme: monokai',
+  components: {
+    codemirror
+  },
+  data() {
+    return {
+      code: dedent`
+  // 以下代码中的方法会被注入到最终的代码中，如果命名与源代码有相同的，则会替换源代码
+  // 内部集成了axios，开发者可以直接通过axios发起网络请求，不过接口需要允许跨域。
+  // axios官方文档：https://www.npmjs.com/package/axios
   export default {
-    name: 'codemirror-example-vue',
-    title: 'Mode: text/x-vue & Theme: monokai',
-    components: {
-      codemirror
-    },
-    data() {
-      return {
-        code: dedent`
-          <template>
-            <h1>Hello World!</h1>
-            <codemirror v-model="code" :options="cmOption" />
-          </template>
+      props: [],
+      components: {},
 
-          <script>
-            // import 'some-codemirror-resource'
-            export default {
-              data() {
-                return {
-                  code: 'const A = 10',
-                  cmOption: {
-                    tabSize: 4,
-                    styleActiveLine: true,
-                    lineNumbers: true,
-                    line: true,
-                    foldGutter: true,
-                    styleSelectedText: true,
-                    mode: 'text/javascript',
-                    keyMap: "sublime",
-                    matchBrackets: true,
-                    showCursorWhenSelecting: true,
-                    theme: "monokai",
-                    extraKeys: { "Ctrl": "autocomplete" },
-                    hintOptions:{
-                      completeSingle: false
-                    }
-                  }
-                }
-              }
-            }
-          ${'<\/script>'}
+      data() {
+        return {
 
-          <style lang="scss">
-            @import './sass/mixins';
-            @import './sass/variables';
-            main {
-              position: relative;
-            }
-          </style>
-        `,
-        cmOption: {
-          tabSize: 4,
-          foldGutter: true,
-          styleActiveLine: true,
-          lineNumbers: true,
-          line: true,
-          keyMap: "sublime",
-          mode: 'text/x-vue',
-          theme: 'monokai',
-          extraKeys: {
-            'F11'(cm) {
-              cm.setOption("fullScreen", !cm.getOption("fullScreen"))
-            },
-            'Esc'(cm) {
-              if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false)
-            }
+        };
+      },
+      watch: {
+
+      },
+
+      computed: {
+
+      },
+
+      methods: {
+        request(){
+          axios.get('https://www.baidu.com').then(res => console.info(res), err => console.error(err));
+        }
+      },
+
+      // 生命周期 start
+      beforeCreate() {},
+      created() {},
+
+      beforeMount() {},
+      mounted() {},
+
+      beforeUpdate() {},
+      updated() {},
+
+      beforeDestory() {},
+      destoryed() {},
+      // 生命周期 end
+
+      fillter: {},
+  };
+      
+      `,
+      cmOption: {
+        tabSize: 4,
+        foldGutter: true,
+        styleActiveLine: true,
+        lineNumbers: true,
+        line: true,
+        keyMap: "sublime",
+        mode: 'text/javascript',
+        theme: 'monokai',
+        extraKeys: {
+          'F11'(cm) {
+            cm.setOption("fullScreen", !cm.getOption("fullScreen"))
+          },
+          'Esc'(cm) {
+            if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false)
           }
         }
       }
+    }
+  },
+  methods: {
+    onCmCursorActivity(codemirror) {
     },
-    methods: {
-      onCmCursorActivity(codemirror) {
-        console.debug('onCmCursorActivity', codemirror)
-      },
-      onCmReady(codemirror) {
-        console.debug('onCmReady', codemirror)
-      },
-      onCmFocus(codemirror) {
-        console.debug('onCmFocus', codemirror)
-      },
-      onCmBlur(codemirror) {
-        console.debug('onCmBlur', codemirror)
-      }
+    onCmReady(codemirror) {
+    },
+    onCmFocus(codemirror) {
+    },
+    onCmBlur(codemirror) {
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .example {
-    display: flex;
-    height: 100%;
+.codemirror,
+.pre {
+  width: 50%;
+  height: 100%;
+  margin: 0;
+  overflow: auto;
+}
 
-    .codemirror,
-    .pre {
-      width: 50%;
-      height: 100%;
-      margin: 0;
-      overflow: auto;
-    }
+.pre {
+  display: block;
+  padding: 1rem;
+  font-size: 14px;
+  line-height: 1.6;
+  word-break: break-all;
+  word-wrap: break-word;
+}
+</style>
 
-    .pre {
-      display: block;
-      padding: 1rem;
-      font-size: 14px;
-      line-height: 1.6;
-      word-break: break-all;
-      word-wrap: break-word;
-    }
-  }
+<style>
+.CodeMirror {
+  height: 100% !important;
+}
+
+.vue-codemirror {
+  height: 100% !important;
+}
 </style>
